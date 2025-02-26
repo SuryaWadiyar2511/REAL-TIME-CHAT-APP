91cefd9a0b347726e6f832471a0a206dbe89c31b
SuryaWadiyar2511 <suryawadiyar@gmail.com>
2025-02-26 21:52:34 +0530

    FRONTEND-SRC

---
+++ App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import Chat from './components/Chat';
+import './App.css';
+import './components/Chat.css';
+
+const App = () => (
+  <div className="app">
+    <Chat />
+  </div>
+);
+
+export default App;
